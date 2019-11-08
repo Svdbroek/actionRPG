c37849dd863361013e345dc4ad0508f78a19beb5
Svdbroek <s_in_the_water@hotmail.com>
2019-11-08 18:51:00 +0100

    created hero object, rest function

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -9,3 +9,24 @@ let hero = {
         damage : 2,
     },
 }
+
+
+
+let rest =(person)=>{
+    if (person.health != 10){
+    person.health = 10
+    return person
+        }else{
+        alert('You are already at full health')
+    }
+
+}
+
+console.log(rest())
+let pickUpItem =()=>{
+
+}
+
+let equipWeapon =()=>{
+
+}
