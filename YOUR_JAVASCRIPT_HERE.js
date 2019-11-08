9745a62914d19d9a0a1076385ddadbca60bc8d3f
Svdbroek <s_in_the_water@hotmail.com>
2019-11-08 18:41:59 +0100

    hero object!

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,11 @@
 // Write your JS here
+let hero = {
+    name : 'Gootsteen the Great',
+    heroic :  false,
+    inventory : [],
+    health : 10,
+    weapon : {
+        type : 'great halbert',
+        damage : 2,
+    },
+}
