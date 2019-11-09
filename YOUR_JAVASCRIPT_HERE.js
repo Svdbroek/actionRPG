6ea6d14a194ba16e7e9dde3cd147642912b17fdd
Svdbroek <s_in_the_water@hotmail.com>
2019-11-09 11:11:16 +0100

    did all the checks, time for css and to flex some muscle

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,32 +1,39 @@
 // Write your JS here
 let hero = {
-    name : 'Gootsteen the Great',
-    heroic :  false,
-    inventory : [],
-    health : 10,
-    weapon : {
-        type : 'great halbert',
-        damage : 2,
-    },
-}
+  name: "Gootsteen the Great",
+  heroic: false,
+  inventory: [],
+  health: 10,
+  weapon: {
+    type: "pointy stick",
+    damage: 2
+  }
+};
 
+let dagger = {
+  type: "dagger",
+  damage: 2
+};
 
+let rest = person => {
+  if (person.health < 10) {
+    person.health = 10;
+  } else {
+    alert("You are already at full health");
+  }
+  return person;
+  // test seems to time out on return-check when alert is triggered, hit that buttton fast :)
+};
 
-let rest =(person)=>{
-    if (person.health != 10){
-    person.health = 10
-    return person
+let pickUpItem = (person, weapon) => {
+  person.inventory.push(weapon);
+};
+
+let equipWeapon = person => {
+        if(person.inventory.length===0)
+        //some type of type-coercion going on if only using one =  ...  not sure why?
+        { 
         }else{
-        alert('You are already at full health')
-    }
-
-}
-
-console.log(rest())
-let pickUpItem =()=>{
-
-}
-
-let equipWeapon =()=>{
-
-}
+        person.weapon=person.inventory[0]
+        }   
+};
