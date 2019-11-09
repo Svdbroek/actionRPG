c5da6104b9179eb5fdee0535b9bdc607687c246e
Svdbroek <s_in_the_water@hotmail.com>
2019-11-09 12:26:35 +0100

    we have a HUD now

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -5,10 +5,31 @@ let hero = {
   inventory: [],
   health: 10,
   weapon: {
-    type: "pointy stick",
+    type: "Pointy stick",
     damage: 2
   }
 };
+
+
+let updateParameters = () => {
+    document.getElementById("name").innerHTML = hero.name;
+    if (hero.heroic) {
+      document.getElementById("heroic").innerHTML = "TRUE HERO";
+    } else {
+      document.getElementById("heroic").innerHTML = "not yet";
+    }
+  
+    let invList = "";
+    for (let i = 0; i < hero.inventory.length; i++) {
+      invList += ", " + hero.inventory[i].type;
+      console.log(invList);
+    }
+    document.getElementById("inventory").innerHTML = invList;
+    document.getElementById("health").innerHTML = hero.health;
+    document.getElementById("weapon").innerHTML = hero.weapon.type;
+    document.getElementById("damage").innerHTML = hero.weapon.damage;
+  }; // probably not elegant, but quick and dirty does the trick
+  
 
 let dagger = {
   type: "dagger",
@@ -18,6 +39,7 @@ let dagger = {
 let rest = person => {
   if (person.health < 10) {
     person.health = 10;
+    updateParameters();
   } else {
     alert("You are already at full health");
   }
@@ -27,13 +49,24 @@ let rest = person => {
 
 let pickUpItem = (person, weapon) => {
   person.inventory.push(weapon);
+  updateParameters();
+
 };
 
 let equipWeapon = person => {
-        if(person.inventory.length===0)
-        //some type of type-coercion going on if only using one =  ...  not sure why?
-        { 
-        }else{
-        person.weapon=person.inventory[0]
-        }   
+  if (person.inventory.length === 0) {
+    //some type of type-coercion going on if only using one =  ...  not sure why?
+  } else {
+    person.weapon = person.inventory[0];
+    
+  updateParameters();
+
+  }
 };
+
+let onStart = () => {
+  hero.name = window.prompt("what's your name?", "Lonk");
+  UIName = hero.name;
+};
+
+onStart();
